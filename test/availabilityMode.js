2b8cacdeb993e332a2233f46fdc8fd290a2d89b6
Jan Polzer <j239p277@ku.edu>
2017-09-12 00:56:17 -0500

    Added availabilityMode.js

---
+++ test/availabilityMode.js
@@ -0,0 +1,9 @@
+//function attendEvent(){
+//	var canAttend = false;
+//	for ( var i, i < eventsArray.length ,i++)
+//        if (eventsArray[i].times  && eventsArray[i].date) {
+//            canAttend = true;
+//        }
+//	return canAttend;
+//}
+
