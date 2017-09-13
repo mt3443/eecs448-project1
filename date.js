000030b3e06b5532f47cb065d44c255f651819e5
Matthew Taylor <mjt@ku.edu>
2017-09-12 20:42:07 -0500

    Added date changing system and moved date.js to root directory

---
+++ date.js
@@ -0,0 +1,79 @@
+var day = new Date();
+
+function monthForward() {
+	day.setMonth(day.getMonth() + 1);
+	updateTime();
+}
+
+function monthBackward() {
+	day.setMonth(day.getMonth() - 1);
+	updateTime();
+}
+
+function dayForward() {
+	day.setDate(day.getDate() + 1);
+	updateTime();
+}
+
+function dayBackward() {
+	day.setDate(day.getDate() - 1);
+	updateTime();
+}
+
+function updateTime()
+{
+
+	var weekDay;
+	var month;
+	
+	
+	//day of the week if's
+	if(day.getDay() == 0) {
+		weekDay = "Sunday"
+	} else if(day.getDay() == 1) {
+		weekDay = "Monday"
+	} else if(day.getDay() == 2) {
+		weekDay = "Tuesday"
+	} else if(day.getDay() == 3) {
+		weekDay = "Wednesday"
+	} else if(day.getDay() == 4) {
+		weekDay = "Thursday"
+	} else if(day.getDay() == 5) {
+		weekDay = "Friday"
+	} else if(day.getDay() == 6) {
+		weekDay = "Saturday"
+	}
+	
+	
+	//month if's
+	if(day.getMonth() == 0) {
+		month = "January "
+	} else if(day.getMonth() == 1) {
+		month = "February "
+	} else if(day.getMonth() == 2) {
+		month = "March "
+	} else if(day.getMonth() == 3) {
+		month = "April "
+	} else if(day.getMonth() == 4) {
+		month = "May "
+	} else if(day.getMonth() == 5) {
+		month = "June "
+	} else if(day.getMonth() == 6) {
+		month = "July "
+	} else if(day.getMonth() == 7) {
+		month = "August "
+	} else if(day.getMonth() == 8) {
+		month = "September "
+	} else if(day.getMonth() == 9) {
+		month = "October "
+	} else if(day.getMonth() == 10) {
+		month = "November "
+	} else if(day.getMonth() == 11) {
+		month = "December "
+	}
+
+
+	document.getElementById("dayOfWeek").innerHTML = weekDay + "";
+	document.getElementById("date").innerHTML = month + day.getDate() + ", " + day.getFullYear();
+
+}
