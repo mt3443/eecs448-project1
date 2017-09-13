47627e0f18789bbd717d5d0ec44cde7c01ab95e5
Matthew Taylor <mjt@ku.edu>
2017-09-12 22:46:43 -0500

    Fixed bug that would duplicate events in each time slot

--- mode_availability.js
+++ mode_availability.js
@@ -16,7 +16,7 @@ function displayEvents() {
 			for(var j = 0; j < eventsArray[i].times.length; j++) { //run through the times that event is occuring
 				for(var k = 0; k < document.getElementsByClassName("time").length; k++) {
 					if(document.getElementsByClassName("time")[k].innerHTML == eventsArray[i].times[j]) {
-						document.getElementsByClassName("t_block")[k].innerHTML += '<div>' + eventsArray[i].name + ', hosted by ' + eventsArray[i].host + '</div>';
+						document.getElementsByClassName("t_block")[k].innerHTML += '<div class="eventDisplay">' + eventsArray[i].name + ', hosted by ' + eventsArray[i].host + '</div>';
 					}
 				}
 			}
