7db713f81f8aeb9a10c22f2bc52fc61ff8811232
Jan Polzer <j239p277@ku.edu>
2017-09-14 00:31:27 -0500

    Fixed highlight today in calendar

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -154,20 +154,19 @@ function drawDays(number_of_days, weekday) {
 		for(var i = 0; i < number_of_days; i++) {
 			if(i === 0) {
 				var margin_left = (weekday + 1) * 26;
-        if (1===1) {  // FIX with d===day or something that works
-          wrapper.innerHTML += '<div style="background-color:#ffffff; margin-left:' + margin_left + 'px;" class="day">' + (i + 1) + '</div>';
+        if (day.getDate()===i+1) {  // Hightlights today's cell in calendar
+          wrapper.innerHTML += '<div style="background-color:#lightblue; margin-left:' + margin_left + 'px;" class="day">' + (i + 1) + '</div>';
         }else{
-          wrapper.innerHTML += '<div style="background-color:#cccccc; margin-left:' + margin_left + 'px;" class="day">' + (i + 1) + '</div>';
+          wrapper.innerHTML += '<div style="background-color:#white; margin-left:' + margin_left + 'px;" class="day">' + (i + 1) + '</div>';
         }
 		}else {
-        if (1===1) {  // FIX with d===day or something that works
-          wrapper.innerHTML += '<div class="day" style="background-color:#ffffff;">' + (i + 1) + '</div>';
+        if (day.getDate()===i+1) {  // Hightlights today's cell in calendar
+          wrapper.innerHTML += '<div class="day" style="background-color:lightblue;">' + (i + 1) + '</div>';
         }else{
-          wrapper.innerHTML += '<div class="day" style="background-color:#cccccc;">' + (i + 1) + '</div>';
+          wrapper.innerHTML += '<div class="day" style="background-color:#white;">' + (i + 1) + '</div>';
         }
 			}
 		}
-    // console.log(d);
 		return;
 }
 
