3a405821a3cf42c1c4583befe069627080e91973
Jan Polzer <j239p277@ku.edu>
2017-09-16 10:08:28 -0500

    Added matching headers to files

--- src/calendar.js
+++ src/calendar.js
@@ -1,18 +1,25 @@
+/*
+==============================
+Filename     : calendar.js
+Authored by  : team "Team"
+Last Updated : 09 12 17
+==============================
+*/
 var days_of_the_week = '<div class="days_of_the_week">Su</div><div class="days_of_the_week">M</div><div class="days_of_the_week">T</div><div class="days_of_the_week">W</div><div class="days_of_the_week">T</div><div class="days_of_the_week">F</div><div class="days_of_the_week">S</div>';
 
 function Calendar() {
 
   var month = monthToString(day.getMonth());
   var tempDate = new Date(day.getFullYear(), day.getMonth(), 1);
-  
+
   if(month == "February") {
-	this.number_of_days = checkLeapYear();  
+	this.number_of_days = checkLeapYear();
   } else if (month == "January" || month == "March" || month == "May" || month == "July" || month == "August" || month == "October" || month == "December") {
 	this.number_of_days = 31;
   } else {
 	this.number_of_days = 30;
   }
-  
+
   this.draw = function() {
 	  var wrapper = document.getElementById('Calendar');
 	  wrapper.style.width = '182px';
@@ -24,20 +31,20 @@ function Calendar() {
 
 function drawDays(month_num, number_of_days, weekday) {
 	var wrapper = document.getElementById('Calendar');
-	
+
 	var today = new Date();
 	var currentMonth = today.getMonth();
 	var currentYear = today.getFullYear();
-	
+
 	for(var i = 1; i <= number_of_days; i++) {
-		
+
 		if(i == 1) {
 			if(weekday == 0) { //wrap around when day is sunday (create a new row for the next week)
 				var margin_left = 0;
 			} else {
 				var margin_left = weekday * 26;
 			}
-       
+
 			if (day.getDate() == i) { //setup for first day of the month (getting month lined up)
 				wrapper.innerHTML += '<div style="color:orange; margin-left:' + margin_left + 'px;" class="day">' + i + '</div>';
 			} else if (day.getDate() == i) {
@@ -84,7 +91,7 @@ function fixCalendarLeft() {
 	removeExistingEvents();
 	displayEvents();
 	cal.draw();
-	
+
 	cal = new Calendar();
 	document.getElementById('Calendar').innerHTML = '';
 	day.setDate(day.getDate() + 1);
@@ -102,7 +109,7 @@ function fixCalendarRight() {
 	removeExistingEvents();
 	displayEvents();
 	cal.draw();
-	
+
 	cal = new Calendar();
 	document.getElementById('Calendar').innerHTML = '';
 	day.setDate(day.getDate() - 1);
--- src/toggleTime.js
+++ src/toggleTime.js
@@ -1,26 +1,33 @@
+/*
+==============================
+Filename     : toggleTime.js
+Authored by  : team "Team"
+Last Updated : 09 12 17
+==============================
+*/
 var timeStyle = 12;
 
 function to24hour(input) {
 	if(input.substr(input.length - 1, 1) != 'm') {
 		return input;
 	}
-	
+
 	if(input == "12:00 am") {
 		return "00:00";
 	}
-	
+
 	if(input == "12:30 am") {
 		return "00:30";
 	}
-	
+
 	if(input == "12:00 pm") {
 		return "12:00";
 	}
-	
+
 	if(input == "12:30 pm") {
 		return "12:30";
 	}
-	
+
 	if(input.substr(input.length - 2,2) == "am") {
 		if(input.charAt(1) == ':') {
 			return "0" + input.substr(0,4);
@@ -42,7 +49,7 @@ function to12hour(input) {
 	if(input.substr(input.length - 1, 1) == "m") {
 		return input;
 	}
-	
+
 	if(input.substr(0,2) < 12) {
 		if(input.substr(0,2) == "00") {
 			return "12" + input.substr(2,3) + " am";
@@ -63,21 +70,21 @@ function to12hour(input) {
 }
 
 function changeTimeStyle(time) {
-	
+
 	var hour;
 	var minutes;
 	var afternoon;
-	
+
 	if(timeStyle == 12) {
-		
+
 		if(time == "12:00 pm") {
 			return "12:00";
 		}
-		
+
 		if(time == "12:30 pm") {
 			return "12:30";
 		}
-		
+
 		if(time.charAt(1) == ':') {
 			hour = time.substr(0,1);
 			minutes = time.substr(2,2);
@@ -87,41 +94,41 @@ function changeTimeStyle(time) {
 			minutes = time.substr(3,2);
 			afternoon = time.substr(6,2) == "pm" ? true : false;
 		}
-		
+
 		if(afternoon && hour != 12) {
 			hour = parseInt(hour) + 12 + "";
 		} else {
 			if(hour < 10) {
 				hour = "0" + hour;
 			}
-			
+
 			if(hour == 12) {
 				hour = "00";
 			}
 		}
-		
+
 		return (hour + ":" + minutes);
-		
+
 	} else {
-	
+
 		hour = time.substr(0,2);
 		minutes = time.substr(3,2);
 		afternoon = hour > 11;
-		
+
 		if(hour < 10) {
 			hour = hour.substr(1,1);
 		}
-		
+
 		if(hour == "0") {
 			hour = "12";
 		}
-		
+
 		if(afternoon && hour != 12) {
 			hour = parseInt(hour) - 12 + "";
 		}
-		
+
 		return hour + ":" + minutes + (afternoon ? " pm" : " am");
-	
+
 	}
 }
 
@@ -133,6 +140,6 @@ var timeArray = document.getElementsByClassName("time");
 	for(var i = 0; i < timeArray.length; i++) {
 		timeArray[i].innerHTML = changeTimeStyle(timeArray[i].innerHTML);
 	}
-	
+
 	timeStyle = (timeStyle == 12 ? 24 : 12);
 }
